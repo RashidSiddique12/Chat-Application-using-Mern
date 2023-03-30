285a4c4a341caa0077bbbd26093a7ccfc442091f
RashidSiddique12 <stylorashid786@gmail.com.com>
2023-03-30 23:16:59 +0530

    day 2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+// const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { tyep: String, required: true, unique: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     pic: {
       type: String,
