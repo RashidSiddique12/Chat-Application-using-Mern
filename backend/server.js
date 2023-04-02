baf8347e4b61be45ac9cd9eafc58fcb1cf13ccde
RashidSiddique12 <stylorashid786@gmail.com.com>
2023-04-03 00:29:26 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,23 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
 connectDB();
 const app = express();
+
+app.use(express.json()); //to accept json data
+
 app.get("/", (req, res) => {
   res.send("API is Running on 5000");
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is started on ${PORT}`.yellow.bold));
