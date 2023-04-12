e0842039b69ed70bee081a5571dffd2a407041c3
RashidSiddique12 <stylorashid786@gmail.com.com>
2023-04-12 23:43:40 +0530

    commit "Created UI"

--- src/pages/Chatpage.js
+++ src/pages/Chatpage.js
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import ChatBox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
-function Chatpage() {
-  const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-    // console.log({ data });
-    setChats(data);
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+  console.log(user);
 
   return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {/* {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )} */}
+      </Box>
     </div>
   );
-}
+};
 
 export default Chatpage;
